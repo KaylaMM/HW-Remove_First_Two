ea21bcc3e7fe0d1e4ad2245bd70b85a08cc9ea5e
Kayla Martinez <kaylamartinez1201@gmail.com>
2020-01-15 23:28:35 -0500

    First Commit, second hw

---
+++ HW-Concat_First-Second_Diagonal.js
@@ -0,0 +1,23 @@
+function concatFirstDiagonal(twoDimArray) {
+    // your code goes here
+    // index of array I'm in matches the index of the string I need
+    let newArr = []
+  for(i=0;i < twoDimArray.length; i++){
+    if(i === twoDimArray[i][i]){
+      return newArr.push();
+    }
+  }
+}
+
+const someArr1 = [
+  ['L','z','x','C','v','B'],
+  ['t','i','I','p','e','7'],
+  ['w','y','s','r','m','x'],
+  ['q','U','l','b','k','H'],
+  ['A','i','G','Y','o','L'],
+  ['n','f','d','W','z','n']
+]
+
+// solution:
+console.log(concatFirstDiagonal(someArr1));
+// ==> "Lisbon"
